ec54c7821b8919a87037c00685729dbb38f4bb39
Marlon Liemann <awesomemarlon@gmail.com>
2021-08-22 10:49:58 -0400

    added comments

--- routes/publicroute.js
+++ routes/publicroute.js
@@ -1,16 +1,19 @@
 const router = require("express").Router();
 const path = require("path");
 
+// Direct to public exercise page
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
-  });
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
+//   direct to main page
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
-  
-  router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
-  });
-  
-  module.exports = router;
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+// direct to public stats
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
