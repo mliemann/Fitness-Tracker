d3e30b6be27089e655d206224f10c42a79e96b7f
Marlon Liemann <awesomemarlon@gmail.com>
2021-08-22 10:48:55 -0400

    created initial routes

---
+++ routes/api.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const db = require("../models/workoutmod");
+
+// Add exercises to the most recent workout plan.
+router.put("/api/workouts/:id", (req, res) => {
+
+});
+// Add new exercises to a new workout plan.
+router.post("/api/workouts", (req, res) => {
+
+});
+// View the combined weight of multiple exercises from the past seven workouts on the stats page.
+router.get("/api/workouts", (req, res) => {
+
+});
+// View the total duration of each workout from the past seven workouts on the stats page.
+
+router.get("/api/workouts/days", (req, res) => {
+
+  });
+  
